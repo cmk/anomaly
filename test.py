2f78a087d0d307d6ddcc5d4ce21d5030f1688f47
cmk <you@example.com>
2022-08-12 18:44:46 -0700

    Add basic gif replay.

--- test.py
+++ test.py
@@ -20,23 +20,34 @@ overlay[:150, 200:] = (255, 0, 0, 64) # reddish
 overlay[150:, :200] = (0, 255, 0, 64) # greenish
 overlay[150:, 200:] = (0, 0, 255, 64) # blueish
 
-width = 256
-height = 128
+width = 512
+height = 256
 
 # Load the arbitrarily sized image
-img = Image.open('kitten2.gif')
+img = Image.open('a1.gif')
 # Create an image padded to the required size with
 # mode 'RGB'
 pad = Image.new('RGBA', (
     ((img.size[0] + width-1) // width) * width,
     ((img.size[1] + height-1) // height) * height,
     ))
-# Paste the original image into the padded one
-pad.paste(img, (0, 0))
+
+while 1:
+    
+    frameIdx = img.tell()
+    print(frameIdx)
+    if frameIdx >= img.n_frames-1:
+        img.seek(0)
+        frameIdx = 0
+    
+    # Paste the original image into the padded one
+    pad.paste(img, (300,32))
+    img.seek(frameIdx+1)
 
 # Add the overlay with the padded image as the source,
 # but the original image's dimensions
-picam2.set_overlay(np.array(pad))
+    picam2.set_overlay(np.array(pad))
+    
 # By default, the overlay is in layer 0, beneath the
 # preview (which defaults to layer 2). Here we make
 # the new overlay semi-transparent, then move it above
