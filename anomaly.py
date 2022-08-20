0f0da214f7436dcda44bcb2f76acdd13dbb87ef2
cmk <you@example.com>
2022-08-19 22:36:55 -0700

    Implement basic anomalous behavior.

--- anomaly.py
+++ anomaly.py
@@ -3,13 +3,30 @@
 from picamera2 import Picamera2, Preview
 from picamera2.encoders import H264Encoder
 import time
-import libcamera
+# import libcamera as lc
 import numpy as np
+import serial as sp
+import random as rng
 
 # import cv2 as cv2
 from PIL import Image, GifImagePlugin
 
+path = '/home/cmk/Documents/anomaly/gifs/'
+GifImagePlugin.LOADING_STRATEGY = 2
 
+#width = 1920
+#height = 1080
+
+width = 1920 // 8
+height = 1080 // 8
+
+# Load the images
+intro = Image.open(path + 'intro.gif')
+src1 = Image.open(path + 'A1.gif')
+src2 = Image.open(path + 'A2.gif')
+#src3 = Image.open('gifs/A3.gif')
+
+# mask = Image.open('mask.png')
 
 # TODO
 #  - use EventHandlers and callbacks to draw gifs
@@ -19,14 +36,12 @@ from PIL import Image, GifImagePlugin
 # Sleep to allow X11 to finish booting
 # time.sleep(20)
 
-GifImagePlugin.LOADING_STRATEGY = 2
-width = 1920
-height = 1080
+rng.seed()
+compass = sp.Serial(port = "/dev/ttyACM0", baudrate=9600, bytesize=8, timeout=2)
+# serialPort.open()
+
 
 picam2 = Picamera2()
-
-
-
 config = picam2.create_preview_configuration(main={"size": (width,height)})
 #config["transform"] = libcamera.Transform(hflip=1) #dont need
 
@@ -41,23 +56,11 @@ picam2.start()
 #picam2.stfart(show_preview=True)
 
 
-#overlay[:150, 200:] = (255, 0, 0, 64) # reddish
-#overlay[150:, :200] = (0, 255, 0, 64) # greenish
-#overlay[150:, 200:] = (0, 0, 255, 64) # blueish
-
-
-blank = np.zeros((height, width, 4), dtype=np.uint8)
-blank[:,:] = (0,0,0,0)
+# blank = np.zeros((height, width, 4), dtype=np.uint8)
+# blank[:,:] = (0,0,0,0)
 # blank[500:, :500] = (255, 0, 0, 128)
 # picam2.set_overlay(blank)
 
-path = '/home/cmk/Documents/anomaly/gifs/'
-# Load the arbitrarily sized image
-src1 = Image.open(path+'A1.gif')
-#src2 = Image.open('gifs/a2.gif')
-#src3 = Image.open('gifs/A3.gif')
-
-# mask = Image.open('mask.png')
 
 
 def playFrame(src, start=0):
@@ -79,45 +82,62 @@ def playFrame(src, start=0):
     src.seek(frameIdx+1)
 
 
-def playGif(src, loops=1, intro=20):
+def playGif(src, loops=1, intro=0):
     
-   for i in range(0, min(intro, src.n_frames)):
-       playFrame(src)
+    for i in range(0, max(intro, src.n_frames)):
+        playFrame(src)
        
-        
-   picam2.set_overlay(None) 
+    picam2.set_overlay(None) 
 
+def playSeq(src, intro, loops=1):
+    
+    playGif(intro, 1)
+    playGif(src, loops)
+
+def getHeading(port):
+    
+    try:
+        raw = port.readline().rstrip()
+        return int(raw[0:4].decode("utf-8"))
+    
+    except:
+        return 60 # 2 o'clock
+
+def close(h1, h2, tol=20):
+    
+    return abs(h1 - h2) < tol
+
+def maybe(default, x):
+    
+    try: x
+        
+    except: default
+        
+lower = 320
+upper = lower + 100
+trigger = 0
+
+while 1:
+    
+    if(compass.in_waiting > 0):
+        
+        time.sleep(0.1)
+        heading = getHeading(compass)
+        print(heading)
+        
+        if close(trigger, heading):
+            
+            print("Playing Gif sequence")
+            playSeq(src2, intro, 4)
+            trigger = rng.randint(lower, upper) % 360
+            print("new trigger")
+            print(trigger)
+            print("done")
+            
+        
         
 # playFrame(src1)
-# time.sleep(20)
-   
-        ## pad.alpha_composite(src)
-        # Paste the original image into the padded one
-
-    
-#src.info['transparency'] = 255
-#src.info['background'] = 0
-#src.convert('RGBA')
-
-#pad.paste(src) #, (0, 0, 540, 540))
-#picam2.set_overlay(np.array(pad))
-#overlay = picam2.set_overlay(np.array(mask))
-#overlay.window = (0, 0, 400, 400)
-
-
-#     
-# # By default, the overlay is in layer 0, beneath the
-# preview (which defaults to layer 2). Here we make
-# the new overlay semi-transparent, then move it above
-# the preview
-# o.alpha = 255
-# o.layer = 3
-
-# overlay = cv2.im
-
-#     
-# # By default, the overlay is in layer 0, beneath the
-# preview (which defaults to layer 2). Here we make
-# the new overlay semi-transparent, then move it above
-# the previewread("kitten.gif", cv2.IMREAD_UNCHANGED)
-# picam2.set_overlay(overlay)
+# time.sleep(60)
+# 
+            
+        
