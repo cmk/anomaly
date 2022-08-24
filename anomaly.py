d4b6e1007f666a694ede6ed3d527e77d04c99677
cmk <you@example.com>
2022-08-23 21:06:41 -0700

    Finalize gifs

--- anomaly.py
+++ anomaly.py
@@ -32,16 +32,16 @@ lores = {"size": (camw,camh), "format": "YUV420"}
 
 # Compass variables        
 offset = 0
-fovmax = 360 # field of view
+fovmax = 120 # field of view
 midpoint = offset + fovmax // 2
-margin = 90
+margin = 8
 trigger = 0
 
 # Load the images
 intro = Image.open(path + 'intro.gif')
 gifs = []
 
-for i in range(0, 33):
+for i in range(0, 31):
     gif = Image.open(path + 'A' + str(i) + '.gif')
     gifs.append(gif)
 
@@ -53,11 +53,11 @@ for i in range(0, 33):
 #    python should only process/insert images when triggered: picam2.post_callback = playFrame ?
 
 # Sleep to allow X11 to finish booting
-# time.sleep(20)
+time.sleep(10)
 
 
 gn.seed()
-compass = sp.Serial(port = "/dev/ttyACM0", baudrate=9600, bytesize=8, timeout=2)
+    
 # serialPort.open()
 
 
@@ -65,9 +65,9 @@ picam2 = Picamera2()
 config = picam2.create_preview_configuration(main=lores)
 
 picam2.configure(config)
-# picam2.start_preview(Preview.DRM, width=pixw, height=pixh)
+picam2.start_preview(Preview.DRM, x=150, width=pixw, height=pixh)
 # picam2.start_preview(Preview.QTGL, width=pixw, height=pixh)
-picam2.start_preview(Preview.QTGL, width=1920, height=1080)
+# picam2.start_preview(Preview.QTGL, width=1920, height=1080)
 picam2.start()
 
 #picam2.configure("preview")
@@ -86,20 +86,23 @@ def window(phi):
     return min(fovmax, abs(phi))
 
 def travel(phi):
+    
+    psi = gn.randint(midpoint + margin, fovmax)
 
     if phi > midpoint:
-        return gn.randint(0, midpoint - margin)
+        psi = gn.randint(0, midpoint - margin)
 
-    return gn.randint(midpoint + margin, fovmax)
+    print("psi: ")
+    print(psi)
+    
+    return  window(psi) + offset
 
 def getHeading(port):
     
-    try:
-        raw = port.readline().rstrip()
-        return int(raw[0:4].decode("utf-8")) + offset
-    
-    except:
-        return midpoint
+    raw = port.readline().rstrip()
+    proc = window(int(raw[0:4].decode("utf-8")))
+    return proc + offset
+
 
 def close(h1, h2):
     
@@ -120,18 +123,49 @@ def playFrame(gif, loc=center):
     gif.seek(frameIdx+1)
 
 
-def playGif(gif, loops=1, loc=center):
+def playGif(gif, loc=center):
     
     for i in range(0, max(10, gif.n_frames)):
         playFrame(gif)
        
     picam2.set_overlay(None) 
 
-def playSeq(gif, intro, loops=1):
+def playSeq(gifs, loc=center):
     
-    loc = (pixw//2 - 250 + gn.randint(-40, 40),pixh//2 - 250 + gn.randint(-20, 20))
-    playGif(intro, 1, loc)
-    playGif(gif, loops, loc)
+    gif = gn.choice(gifs)
+    playGif(intro, loc)
+    playGif(gif, loc)
+
+def anomaly(heading, trigger):
+
+    if close(trigger, heading):
+        gif = gn.choice(gifs)
+        loc = (pixw//2 - 250 + gn.randint(-40, 40),pixh//2 - 250 + gn.randint(-20, 20))
+        playSeq(gif, loc)
+        trigger = travel(heading)
+        print("new trigger")
+        print(trigger)
+        print("done")
+
+def fallback():
+
+    gif = gn.choice(gifs)
+    loc = (gn.randint(0, 1000000) % pixw, gn.randint(0, 1000000) % pixh)
+    playSeq(gifs, loc)
+    period = gn.randint(20, 35)
+    # time.sleep(period)
+    time.sleep(1)
+
+
+try:
+    try:
+        compass = sp.Serial(port = "/dev/ttyACM0", baudrate=9600, bytesize=8, timeout=2)
+
+    except:
+        compass = sp.Serial(port = "/dev/ttyACM1", baudrate=9600, bytesize=8, timeout=2)
+
+except:
+    fallback()
 
 # time.sleep(5)
 # gif = gn.choice(gifs)
@@ -143,17 +177,14 @@ while 1:
 
     if (keyboard.is_pressed('q')):
         sys.exit()
-     
-    if(compass.in_waiting > 0):
+    
+    try:
+        assert compass.in_waiting > 0, "compass unavailable"
         heading = getHeading(compass)
         print(heading)
-        
-        if close(trigger, heading):
-            
-            print("Playing Gif sequence")
-            gif = gn.choice(gifs)
-            playSeq(gif, intro, 1)
-            trigger = travel(heading)
-            print("new trigger")
-            print(trigger)
-            print("done")
+        anomaly(heading, trigger) 
+
+    except:
+        if (keyboard.is_pressed('q')):
+            sys.exit()
+        fallback()
